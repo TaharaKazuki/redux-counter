9a3099a085449679c845f1fca42c1cad24a5afb5
田原　一樹 <k.tahara@leihauoli.com>
2017-09-19 09:25:39 +0900

    機能実装

---
+++ src/components/Counter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+	render(){
+		const { value, onIncrement, onDecrement } = this.props;
+		console.log(this.props);
+		return (
+			<div>
+				<h3>Counter:{value}</h3>
+				<button onClick={onIncrement}>
+					+
+				</button>
+				{' '}
+				<button onClick={onDecrement}>
+					-
+				</button>
+			</div>			
+		);
+	}
+}
