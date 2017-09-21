8439b8d52e9359a9ac78c09eb1aa92a31963a207
田原　一樹 <k.tahara@leihauoli.com>
2017-09-22 00:39:37 +0900

    indent修正

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,11 +7,11 @@ export default class Counter extends Component {
 	  <div>
 		<h3>Counter:{value}</h3>
 		<button onClick={onIncrement}>
-			+
+		  +
 		</button>
 		{' '}
 		<button onClick={onDecrement}>
-			-
+		  -
 		</button>
 	  </div>
 	);
